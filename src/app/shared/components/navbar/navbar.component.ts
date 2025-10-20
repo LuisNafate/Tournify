3d1d6730464c17e54918f953d18a55614feb5e97
LuisNafate <luisnafate51@gmail.com>
2025-10-20 15:04:00 -0600

    feat: crear componentes compartidos (Navbar y Footer)

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// Componente de navegación
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+}
