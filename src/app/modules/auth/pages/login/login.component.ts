1c420a5a710a76851c1e7b54424b0726685583f1
LuisNafate <luisnafate51@gmail.com>
2025-10-20 17:07:00 -0600

    feat: crear módulo Auth con Login y Register

---
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// Componente de login
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+}
---
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// Componente de registro
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+
+}
