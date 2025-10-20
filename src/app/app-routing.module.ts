a36a2df37d7419f91c8f02a05cae2e3cca88d390
LuisNafate <luisnafate51@gmail.com>
2025-10-20 14:04:00 -0600

    feat: configurar módulo raíz, componente principal y sistema de rutas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Rutas principales con lazy loading
+const routes: Routes = [
+  { 
+    path: '', 
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) 
+  },
+  { 
+    path: 'auth', 
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) 
+  },
+  { 
+    path: 'tournaments', 
+    loadChildren: () => import('./modules/tournaments/tournaments.module').then(m => m.TournamentsModule) 
+  },
+  { 
+    path: '**', 
+    redirectTo: '' 
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
