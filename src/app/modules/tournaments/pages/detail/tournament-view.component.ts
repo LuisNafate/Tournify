d3616fe06e8caa9458a4957d684872c0a55affc6
LuisNafate <luisnafate51@gmail.com>
2025-10-29 16:07:00 -0600

    feat: crear componente typescript tournament-view

---
+++ src/app/modules/tournaments/pages/detail/tournament-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// componente para vista del torneo
+@Component({
+  selector: 'app-tournament-view',
+  templateUrl: './tournament-view.component.html',
+  styleUrls: ['./tournament-view.component.css']
+})
+export class TournamentViewComponent {
+
+}
